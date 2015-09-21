879f08a592e06e4ddeaf7e726e5be152b3f239d2
Khang Minh <kminhfoss@gmail.com>
2015-09-21 20:59:59 +0700

    fix: Fix WP version in plugin framework testcase [ci]

--- src/phpunit/unit-testcase.php
+++ src/phpunit/unit-testcase.php
@@ -1,5 +1,7 @@
 <?php
 
+use \Mockery as Mockery;
+
 /**
  * Copyright (c) 2015 Khang Minh <contact@betterwp.net>
  * @license http://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE VERSION 3.0 OR LATER
@@ -8,7 +10,7 @@
 /**
  * @author Khang Minh <contact@betterwp.net>
  */
-abstract class BWP_Framework_PHPUnit_Unit_TestCase extends PHPUnit_Framework_TestCase
+abstract class BWP_Framework_PHPUnit_Unit_TestCase extends \PHPUnit_Framework_TestCase
 {
 	protected $bridge;
 
@@ -18,15 +20,21 @@ abstract class BWP_Framework_PHPUnit_Unit_TestCase extends PHPUnit_Framework_Tes
 	{
 		$this->bridge = Mockery::mock('BWP_WP_Bridge');
 
-		$this->bridge->shouldReceive('get_bloginfo')->andReturn('3.0')->byDefault();
+		$this->bridge->shouldReceive('get_bloginfo')->andReturn('4.3')->byDefault();
 		$this->bridge->shouldReceive('get_option')->andReturn(false)->byDefault();
 		$this->bridge->shouldReceive('update_option')->byDefault();
 
 		$this->bridge->shouldReceive('do_action')->andReturnNull()->byDefault();
 		$this->bridge->shouldReceive('add_action')->byDefault();
-		$this->bridge->shouldReceive('apply_filters')->byDefault();
+		$this->bridge->shouldReceive('apply_filters')->andReturnUsing(function($hook_name, $value) {
+			return $value;
+		})->byDefault();
 		$this->bridge->shouldReceive('apply_filters')->with('/[a-z_]+_default_options/', array())->andReturn(array())->byDefault();
 		$this->bridge->shouldReceive('add_filter')->byDefault();
+
+		$this->bridge->shouldReceive('trailingslashit')->andReturnUsing(function($path) {
+			return rtrim($path, '/') . '/';
+		})->byDefault();
 
 		$this->bridge->shouldReceive('is_admin')->andReturn(false)->byDefault();
 
