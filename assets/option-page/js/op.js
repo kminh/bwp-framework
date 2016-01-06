cfb47148752914e0d6ecacc782908cb51db843d6
Khang Minh <kminhfoss@gmail.com>
2016-01-06 21:28:14 +0700

    new: Add comment counts/views to BWP Gems, and add post date and "Just in!" text to BWP News.

--- assets/option-page/js/op.js
+++ assets/option-page/js/op.js
@@ -20,12 +20,50 @@ jQuery(function($) {
 				var $li = $('<li>');
 
 				$('<a>', { text: post.title, })
+					.attr('target', '_blank')
 					.attr('href', post.permalink
 						+ '?utm_source=' + container.data('pluginKey')
 						+ '&utm_medium=feed'
-						+ '&utm_campaign=sidebar-2015'
+						+ '&utm_campaign=sidebar-2016'
+						+ (post.just_in ? '&utm_content=justin' : '')
 					)
 					.appendTo($li);
+
+				// indicate fresh articles
+				if (post.just_in) {
+					$('<span />', {
+						'class': 'bwp-justin',
+						text: ' Just in!'
+					})
+					.appendTo($li)
+				}
+
+				$('<br />').appendTo($li);
+
+				if (post.views) {
+					var span_tpl = post.comment_count
+						? ''
+							+ parseInt(post.comment_count, 10)
+							+ ' '
+							+ '<span class="bwp-meta">comments</span>'
+							+ '<span class="bwp-meta"> / </span>'
+						: '';
+
+					span_tpl = span_tpl
+						+ parseInt(post.views / 1000, 10)
+						+ 'k+ <span class="bwp-meta">views</span>';
+
+					$('<span />', {
+						html: span_tpl
+					})
+					.appendTo($li);
+				} else {
+					$('<span />', {
+						'class': 'bwp-meta',
+						text: post.post_date_gmt
+					})
+					.appendTo($li);
+				}
 
 				$li.appendTo(container.find('.bwp-feed'));
 			});
